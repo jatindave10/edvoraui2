a30a8bb0a6e03bcad6e2010b8aa16b8cbd1921ce
Jatin Dave <davejatin09@gmail.com>
2021-11-19 12:46:12 +0530

    Initial Commit

---
+++ edvoraui2/edvoraui2App.swift
@@ -0,0 +1,17 @@
+//
+//  edvoraui2App.swift
+//  edvoraui2
+//
+//  Created by Jatin Dave on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct edvoraui2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
